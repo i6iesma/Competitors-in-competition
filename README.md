e118bf599ebc00177aa1ebd216cba925addbdb6e
Iñigo <inigo@escolar.net>
2024-07-10 19:46:12 +0200

    changed url to come after /competitions

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 ## A rest api to get the competitors in a competition **that has not happened yet** using web scraping
 **Live on https://competitors-in-competition.vercel.app**
 # Usage
-Go to competitors-in-competition.vercel/{the competition Id you want to get the competitors from}  
-e.g. https://competitors-in-competition.vercel.app/Euro2024
+Go to competitors-in-competition.vercel/competitions/{the competition Id you want to get the competitors from}  
+e.g. https://competitors-in-competition.vercel.app/competitions/Euro2024
 
 You can find the competition id by going to the WCA page for the competition and looking at the end of the url
 
 
-https://www.worldcubeassociation.org/competitions/**Euro2024** -> Here Euro2024 is the competition Id for the european championships 2024
+https://www.worldcubeassociation.org/competitions/Euro2024 -> Here Euro2024 is the competition Id for the european championships 2024
 
 
 # Local Setup
--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 const axiosInstance = axios.create({
     baseURL: 'https://www.worldcubeassociation.org',
-  });
+});
 
-app.get('/:competitionId', async (req, res) => {
+app.get('/competitions/:competitionId', async (req, res) => {
     console.time("scrape");
     const competitionId = req.params.competitionId;
     try {
@@ -17,19 +17,19 @@ app.get('/:competitionId', async (req, res) => {
         const html = response.data;
         // Load the HTML into Cheerio
         const $ = cheerio.load(html);
-        
+
         // Extract the data you need
         let idArr = []
         $('table tbody tr').each((index, element) => {
             const href = $(element).find('td').children("a").attr("href");
             if (href != undefined) {
-            const id = href.split("/").pop();
-            idArr.push(id);
-            } 
-            
-            
+                const id = href.split("/").pop();
+                idArr.push(id);
+            }
+
+
         });
-        
+
         // Send the extracted data as JSON
         res.json(idArr);
     } catch (error) {
